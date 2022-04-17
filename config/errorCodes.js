d793e1d2613eb1ef0c23ddb5b5d2d6d57e157f27
Minh Nguyen <xiuquay55@gmail.com>
2022-04-17 23:02:05 +0700

    modify code

--- config/errorCodes.js
+++ config/errorCodes.js
@@ -1,19 +1,24 @@
 'use strict';
 
 const errorCodes = {
+  TokenNotFound: {
+    message: 'Token not found',
+    returnCode: 2001,
+    statusCode: 401
+  },
   TokenExpired: {
     message: 'Jwt expired',
-    returnCode: 2001,
+    returnCode: 2002,
     statusCode: 401
   },
   TokenInvalid: {
     message: 'Token invalid',
-    returnCode: 2002,
+    returnCode: 2003,
     statusCode: 401
   },
   TokenForbidden: {
     message: 'Method has been banned',
-    returnCode: 2003,
+    returnCode: 2004,
     statusCode: 403
   }
 };
